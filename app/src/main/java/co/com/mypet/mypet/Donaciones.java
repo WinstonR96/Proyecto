6557ca0d3d739d6b9f19fbeb6e69fd6674591a58
Mauricio Onoro <mauricioonoro@gmail.com>
2018-11-22 08:39:33 -0500

    Ventana donacion

---
+++ app/src/main/java/co/com/mypet/mypet/Donaciones.java
@@ -0,0 +1,13 @@
+package co.com.mypet.mypet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Donaciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donaciones);
+    }
+}
